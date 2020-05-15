3ab888440c1bbb7baab2e8d0f5e4ef552e17f2cc
Alexander Karpov <kukuruku@yandex-team.ru>
2020-05-16 00:24:07 +0300

    Добавляет новые сцены в диалог

--- src/repka.ts
+++ src/repka.ts
@@ -1,15 +1,22 @@
 import { DialogBuilder } from './DialogBuilder/DialogBuilder';
 import { RepkaState } from './RepkaState';
 import { RepkaScreen } from './RepkaScreen';
+import { Character } from './Character';
 import { configureEntryPoint } from './screens/configureEntryPoint';
 import { configureGreating } from './screens/configureGreating';
 import { configureQuit } from './screens/configureQuit';
-import { Character } from './Character';
+import { configureBeginOfTale } from './screens/configureBeginOfTale';
+import { configureTaleChain } from './screens/configureTaleChain';
+import { configureCallСharacter } from './screens/configureCallСharacter';
 
 const dialogBuilder = new DialogBuilder<RepkaState, RepkaScreen>();
+
 configureEntryPoint(dialogBuilder.createScreen(RepkaScreen.EntryPoint));
 configureGreating(dialogBuilder.createScreen(RepkaScreen.Greating));
 configureQuit(dialogBuilder.createScreen(RepkaScreen.Quit));
+configureBeginOfTale(dialogBuilder.createScreen(RepkaScreen.BeginOfTale));
+configureTaleChain(dialogBuilder.createScreen(RepkaScreen.TaleChain));
+configureCallСharacter(dialogBuilder.createScreen(RepkaScreen.CallСharacter));
 
 export const repka = dialogBuilder.build(RepkaScreen.EntryPoint, {
     characters: [Character.dedka],
