52032657da7e6f4e6fbad52861dd110444778212
Alexander Karpov <kukuruku@yandex-team.ru>
2020-10-24 05:21:28 +0300

    Выделяет работу с дампами в отдельный файл.

--- src/repka/stemmer/DumpingStemmer.ts
+++ src/repka/stemmer/DumpingStemmer.ts
@@ -1,41 +1,21 @@
 import { Stemmer } from './Stemmer';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as crypto from 'crypto';
-import { Token } from "./Token";
+import { Token } from './Token';
+import { DumpService } from './DumpService';
 
 /**
  * Добавляет кэширование на файловую систему
  */
 export class DumpingStemmer implements Stemmer {
-    constructor(private readonly stemmer: Stemmer) {}
+    private readonly dumpService: DumpService;
 
-    async analyze(message: string): Promise<Token[]> {
-        const dumpFileName = this.makeDumpFileName(message);
-        const dumpPath = path.resolve('/tmp', dumpFileName);
-
-        try {
-            if (fs.existsSync(dumpPath)) {
-                const content = fs.readFileSync(dumpPath).toString('utf8');
-                return JSON.parse(content);
-            }
-        } catch (error) {
-            console.error(`DumpingStemmer: не удалось прочитать файл дампа: ${error}.`);
-        }
-
-        const tokens = await this.stemmer.analyze(message);
-
-        try {
-            fs.writeFileSync(dumpPath, JSON.stringify(tokens));
-        } catch (error) {
-            console.error(`DumpingStemmer: не удалось сохранить файл дампа: ${error}.`);
-        }
-
-        return tokens;
+    constructor(private readonly stemmer: Stemmer) {
+        this.dumpService = new DumpService(
+            async (message) => JSON.stringify(await this.stemmer.analyze(message)),
+            'repla-stemmer-'
+        );
     }
 
-    private makeDumpFileName(message: string): string {
-        const hash = crypto.createHash('md5').update(message, 'utf8').digest('hex');
-        return `repka-stem-${hash}`;
+    async analyze(message: string): Promise<Token[]> {
+        return JSON.parse(await this.dumpService.get(message)) as Token[];
     }
 }
