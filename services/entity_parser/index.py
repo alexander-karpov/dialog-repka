ca49f29972a517fc647577da0ef0004db3bad898
Alexander Karpov <kukuruku@yandex-team.ru>
2022-04-05 05:30:45 +0300

    Обогащение в сервисе сущностей

--- services/entity_parser/index.py
+++ services/entity_parser/index.py
@@ -1,6 +1,13 @@
 from functools import lru_cache
 from entity_parser import entity_parser
 import json
+import dawg
+import random
+from morph import inflect, morph
+
+
+completion_dawg = dawg.CompletionDAWG()
+completion_dawg.load('./adj.dawg')
 
 
 @lru_cache(2048)
@@ -19,15 +26,66 @@ def parser(text: str) -> str:
             'tags': str(subject.tag)
         })
 
-    return json.dumps(response)
+    return response
+
+
+def has_one_word(entity) -> bool:
+    return entity['nomn'].find(' ') == -1
+
+
+def enrich_entity(entity) -> None:
+    if not has_one_word(entity):
+        return
+
+    res = find_adj(morph.parse(entity["nomn"]))
+
+    if not res:
+        return
+
+    adj_nomn, tag = res
+    adj_accs, _ = inflect(adj_nomn, [{ 'accs', tag.gender, tag.number }])
+
+    if adj_nomn and adj_accs:
+        entity["enriched_nomn"] = f"{adj_nomn} {entity['nomn']}"
+        entity["enriched_accs"] = f"{adj_accs} {entity['accs']}"
+
+
+def find_adj(parsed_word) -> str:
+    for p in parsed_word:
+        if "NOUN" in p.tag:
+            # Во избежание странных слов из одной буквы
+            if len(p.normal_form) < 3:
+                return None
+
+            cases = completion_dawg.keys(f"{p.normal_form}:")
+
+            if not len(cases):
+                return None
+
+            selected = random.choice(cases)
+            _, adj = selected.split(':')
+
+            adj_inf, _ = inflect(adj, [{ p.tag.gender, p.tag.case, p.tag.number }])
+
+            return adj_inf, p.tag if adj_inf else None
+
+    return None
 
 
 def handler(event, context):
+    entities = parser(event['queryStringParameters']['text'])
+
+    try:
+        for entity in entities:
+            enrich_entity(entity)
+    except Exception:
+        print('Ошибка в enrich_entity')
+
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'application/json'
         },
         'isBase64Encoded': False,
-        'body': parser(event['queryStringParameters']['text'])
+        'body': json.dumps(entities)
     }
